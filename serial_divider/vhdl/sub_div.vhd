231068a6f54bd51ea67f9d4d188530b765d01825
up-stairs <merdiven@yaani.com>
2023-12-10 22:03:17 +0300

    Update sub_div.vhd
    New ports added + bug fix

--- serial_divider/vhdl/sub_div.vhd
+++ serial_divider/vhdl/sub_div.vhd
@@ -8,21 +8,21 @@ entity sub_div is
     DD        : natural := 7;  -- Replace with the actual dividend bit width
     DR        : natural := 7;  -- Replace with the actual dividend bit width
     FR        : natural := 0;  -- Replace with the actual fractional part length
-    DIV_STEP  : natural := 0  -- 
+    DIV_STEP  : natural := 0  --
   );
   port (
     clock         : in std_logic;                 -- Clock input
     reset         : in std_logic;                -- Reset input
     clken         : in std_logic;                -- Reset input
-    
+
     valid_in      : in std_logic;
-    user_in       : in std_logic_vector(UW-1 downto 0);   -- 
+    user_in       : in std_logic_vector(UW-1 downto 0);   --
     dividend_in   : in std_logic_vector(DD+FR-1 downto 0);   -- Input dividend
     divisor_in    : in std_logic_vector(DR+DD+FR-1 downto 0);    -- Input divisor
     quotent_in    : in std_logic_vector(DD+FR-1 downto 0);    -- Input quotient
-    
+
     valid_out     : out std_logic;
-    user_out      : out std_logic_vector(UW-1 downto 0);   -- 
+    user_out      : out std_logic_vector(UW-1 downto 0);   --
     dividend_out  : out std_logic_vector(DD+FR-1 downto 0);   -- Output dividend
     divisor_out   : out std_logic_vector(DR+DD+FR-1 downto 0);  -- Output divisor
     quotent_out   : out std_logic_vector(DD+FR-1 downto 0)  -- Output quotient
@@ -36,7 +36,7 @@ architecture rtl of sub_div is
   signal temp_divisor   : unsigned(divisor_in'range);
   signal temp_quotent   : std_logic_vector(quotent_in'range);
   signal temp_user      : std_logic_vector(user_in'range);
-  
+
 begin
 
   process (clock, reset)
@@ -50,16 +50,23 @@ begin
       temp_valid    <= '0';
     elsif (rising_edge(clock)) then
       if (clken = '1') then
-        if (unsigned(divisor_in) <= unsigned(dividend_in)) then
-          -- Perform division operation
-          temp_dividend       <= unsigned(dividend_in) - unsigned(divisor_in(dividend_in'range));
-          temp_quotent        <= quotent_in;
-          temp_quotent(DIV_STEP)  <= '1';
-        end if;
-        
+
         temp_valid      <= valid_in;
-        temp_user       <= user_in;
-        temp_divisor    <= shift_left(shift_right(unsigned(divisor_in), 1), 1);
+
+        if (valid_in = '1') then
+          if (unsigned(divisor_in) <= unsigned(dividend_in)) then
+            -- Perform division operation
+            temp_dividend       <= unsigned(dividend_in) - unsigned(divisor_in(dividend_in'range));
+            temp_quotent        <= quotent_in;
+            temp_quotent(DIV_STEP)  <= '1';
+          else
+            temp_dividend       <= unsigned(dividend_in);
+            temp_quotent        <= quotent_in;
+          end if;
+
+          temp_user       <= user_in;
+          temp_divisor    <= shift_left(shift_right(unsigned(divisor_in), 1), 1);
+        end if;
       end if;
     end if;
   end process;
@@ -70,5 +77,5 @@ begin
   quotent_out   <= std_logic_vector(temp_quotent);
   user_out      <= temp_user;
   valid_out     <= temp_valid;
-  
+
 end architecture rtl;
