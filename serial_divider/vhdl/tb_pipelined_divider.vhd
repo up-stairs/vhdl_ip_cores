b9c656102af612e55e72e1b6ce7953ec7530a752
up-stairs <merdiven@yaani.com>
2023-12-10 23:12:40 +0300

    Update tb_pipelined_divider.vhd
    Bug fix + some minor changes

--- serial_divider/vhdl/tb_pipelined_divider.vhd
+++ serial_divider/vhdl/tb_pipelined_divider.vhd
@@ -18,10 +18,10 @@ end tb_pipelined_divider;
 architecture testbench of tb_pipelined_divider is
   constant CPERIOD            : time := 10 ns;
 
-  constant DD                 : natural := 17;  -- Replace with the actual dividend bit width
-  constant DR                 : natural := 17;  -- Replace with the actual dividend bit width
-  constant FR                 : natural := 4;  -- Replace with the actual fractional part length
-  constant UW                 : natural := DD+FR+DR+FR; -- user width
+  constant DD                 : natural := 13;  -- Replace with the actual dividend bit width
+  constant DR                 : natural := 15;  -- Replace with the actual dividend bit width
+  constant FR                 : natural := 0;  -- Replace with the actual fractional part length
+  constant UW                 : natural := DD+FR+DD+FR; -- user width
 
   signal clock                : std_logic;
   signal reset                : std_logic;
@@ -34,10 +34,11 @@ architecture testbench of tb_pipelined_divider is
   signal valid_out            : std_logic;
   signal user_out             : std_logic_vector(UW-1 downto 0);   --
   signal quotent_out          : std_logic_vector(DD+FR-1 downto 0);  -- Output quotient
-  signal remainder_out        : std_logic_vector(DR+FR-1 downto 0);  -- Output remainder
+  signal remainder_out        : std_logic_vector(DD+FR-1 downto 0);  -- Output remainder
 
+  signal tb_mismatch          : std_logic;
   signal tb_quotent           : std_logic_vector(DD+FR-1 downto 0);  -- Output quotient
-  signal tb_remainder         : std_logic_vector(DR+FR-1 downto 0);  -- Output remainder
+  signal tb_remainder         : std_logic_vector(DD+FR-1 downto 0);  -- Output remainder
 begin
 
   process
@@ -51,7 +52,7 @@ begin
   process
   begin
     reset   <= '1';
-    wait for CPERIOD*100;
+    wait for CPERIOD*2;
     reset   <= '0';
     wait;
   end process;
@@ -71,21 +72,23 @@ begin
       wait until falling_edge(clock);
 
       dividend_v    := randi(0, 2**dividend_in'length-1);
-      divisor_v     := randi(1, 2**dividend_in'length-1);
+      divisor_v     := randi(1, 2**divisor_in'length-1);
 
       quotent_v     := (dividend_v*2**FR)/divisor_v;
       remainder_v   := dividend_v*2**FR - quotent_v*divisor_v;
 
       dividend_in   <= std_logic_vector(to_unsigned(dividend_v, dividend_in'length));
       divisor_in    <= std_logic_vector(to_unsigned(divisor_v, divisor_in'length));
-      user_in       <= std_logic_vector(to_unsigned(quotent_v, DD+FR) & to_unsigned(remainder_v, DR+FR));
+      user_in       <= std_logic_vector(to_unsigned(quotent_v, DD+FR) & to_unsigned(remainder_v, DD+FR));
+      
+      -- echo( "quotent_v - remainder_v " & to_string(quotent_v) & " - " & to_string(remainder_v) );
 
 
       valid_in    <= '1';
       wait for CPERIOD;
       valid_in    <= '0';
 
-      wait for CPERIOD*randi(0, 100);
+      wait for CPERIOD*randi(0, 2);
     end loop;
 
     wait;
@@ -118,4 +121,24 @@ begin
   tb_quotent    <= user_out(user_in'left downto tb_remainder'left+1);
   tb_remainder  <= user_out(tb_remainder'range);
 
+  -- clken <= valid_out and valid_in;
+
+  process
+  begin
+
+    while (true) loop
+
+      wait until falling_edge(valid_out);
+
+
+      if (tb_quotent /= quotent_out or tb_remainder /= remainder_out) then
+        tb_mismatch   <= '1';
+      else
+        tb_mismatch   <= '0';
+      end if;
+      
+    end loop;
+
+    wait;
+  end process;
 end testbench;
